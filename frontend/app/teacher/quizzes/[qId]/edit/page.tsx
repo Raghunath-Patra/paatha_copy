14fc0b112e909d1f9957d750847d34122cf42ea6
Raghunath-Patra <raghunathpatra909@gmail.com>
2025-06-21 08:23:25 +0530

    h

--- frontend/app/teacher/quizzes/[qId]/edit/page.tsx
+++ frontend/app/teacher/quizzes/[qId]/edit/page.tsx
@@ -1,1 +1,5 @@
 //Page for teachers to create questions for a quiz for now keep it simple as: he can set question, it's type and options (if mcq).
+// app/teacher/quizzes/[qId]/edit/page.tsx
+export default function EditQuizPage() {
+  return <div>Edit Quiz Page</div>;
+}
